89a7a0120effa9d095677bb6aece3e1c34c92d39
Ehsan <spin313@gmail.com>
2023-04-13 14:58:23 +0330

    tests for invalid id and invalid depot added

--- tspapi_solver/tests.py
+++ tspapi_solver/tests.py
@@ -32,6 +32,41 @@ class VrpSolverTestCase(TestCase):
         self.assertIn("message", response.data)
         self.assertEqual(response.data["message"], "Operation successful")
 
-        # assert that the response result is equal to the request data
+        # assert that the response result contains "result" key
         self.assertIn("result", response.data)
 
+    def test_invalid_id(self):
+        # create an invalid request data with empty ID
+        data = {
+            "id": "",
+            "depot": 0,
+            "num_vehicles": 1,
+            "locations": json.dumps([{"latitude": 40.7128, "longitude": -74.0060}])
+        }
+
+        # post the request to the API endpoint
+        response = self.client.post(self.url, data)
+
+        # assert that the response code is 400 Bad Request
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        # assert that the response message is related to invalid id
+        self.assertIn("id", response.data)
+
+    def test_invalid_depot(self):
+        # create an invalid request data with depot < 0
+        data = {
+            "id": "1234",
+            "depot": -1,
+            "num_vehicles": 1,
+            "locations": [{"latitude": 40.7128, "longitude": -74.0060}]
+        }
+
+        # post the request to the API endpoint
+        response = self.client.post(self.url, data)
+
+        # assert that the response code is 400 Bad Request
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        # assert that the response message is "Invalid input data"
+        self.assertIn("depot", response.data)
