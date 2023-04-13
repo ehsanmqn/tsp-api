e2970f7596a62bda6bb9ad3ed4fc5874301e0490
Ehsan <spin313@gmail.com>
2023-04-13 14:54:31 +0330

    test valid request data added

--- tspapi_solver/tests.py
+++ tspapi_solver/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.urls import reverse
+from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+
+
+class VrpSolverTestCase(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.url = reverse("vrp-solver")
+
+    def test_valid_request_data(self):
+        # create a valid request data
+        data = {
+            "id": "1234",
+            "depot": 0,
+            "num_vehicles": 1,
+            "locations": json.dumps([{"latitude": 40.7128, "longitude": -74.0060}])
+        }
+
+        # post the request to the API endpoint
+        response = self.client.post(self.url, data)
+
+        # assert that the response code is 200 OK
+        self.assertIn("code", response.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # assert that the response message is "Operation successful"
+        self.assertIn("message", response.data)
+        self.assertEqual(response.data["message"], "Operation successful")
+
+        # assert that the response result is equal to the request data
+        self.assertIn("result", response.data)
+
