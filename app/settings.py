eec6230b0646f78f6358fea87e86616aba0b0773
Ehsan <spin313@gmail.com>
2023-04-16 21:04:46 +0330

    CORS_ORIGIN_WHITELIST updated

--- app/settings.py
+++ app/settings.py
@@ -159,10 +159,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-)
+CORS_ORIGIN_WHITELIST = ()
 
 # Application defined constants
 MESSAGE_BROKER = os.environ.get('MESSAGE_BROKER', 'localhost')
